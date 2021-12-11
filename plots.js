3ebbf266870cda010fbfbdee07b80922c375cdb1
Matthew Godfrey <matthewgodfrey@MacBook-Pro.local>
2021-12-11 14:37:36 -0600

    Implemented function stubs and an event handler.

--- plots.js
+++ plots.js
@@ -1,8 +1,35 @@
 // This code is taken from Dom's office hours session on 12/11
 
-console.log("plots.js is running")
+console.log("plots.js loaded")
 
+// Function to draw the barchart
+function drawBarchart(sampleID) {
+    console.log(`DrawBarchart(${sampleID})`);
+}
 
+// Function to draw the bubble chart
+function drawBubblechart(sampleID) {
+    console.log(`DrawBubblechart(${sampleID})`);
+}
+
+// Function to populate the demographic info
+function populateDemographics(sampleID) {
+    console.log(`Showing demographic data ${sampleID}`);
+}
+
+// Event handler
+function optionChanged(id) {
+    console.log(`optionChanged(${id})`);
+
+    // Populate demographic info
+    populateDemographics(id);
+    // Display the barchart
+    drawBarchart(id);
+    // Display the bubble chart
+    drawBubblechart(id);
+}
+
+// Initialize the dashboard
 function InitDashboard() {
     console.log("Initializing dashboard")
 
@@ -17,7 +44,13 @@ function InitDashboard() {
                 .text(sampleID)
                 .property("value", sampleID)
         })
-    })
+
+        let sampleID = sampleNames[0];
+
+        populateDemographics(sampleID);
+        drawBarchart(sampleID);
+        drawBubblechart(sampleID);
+    });
 
 }
 

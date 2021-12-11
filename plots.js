460ec6aa77f404646bb2f262e85a7a15a0d166bd
Matthew Godfrey <matthewgodfrey@MacBook-Pro.local>
2021-12-11 14:54:24 -0600

    Created a functional bar chart in plots.js.

--- plots.js
+++ plots.js
@@ -5,6 +5,38 @@ console.log("plots.js loaded")
 // Function to draw the barchart
 function drawBarchart(sampleID) {
     console.log(`DrawBarchart(${sampleID})`);
+
+    d3.json("samples.json").then(data => {
+        
+        let samples = data.samples;
+        let resultArray = samples.filter(s => s.id === sampleID);
+        let result = resultArray[0];
+        console.log(result);
+
+        let otu_ids = result.otu_ids;
+        let otu_labels = result.otu_labels;
+        let sample_values = result.sample_values;
+
+        console.log(sample_values)
+
+        let yticks = otu_ids.slice(0,10).map(otuID => `OTU${otuID}`).reverse();
+        let barData = {
+            x: sample_values.slice(0,10).reverse(),
+            y: yticks,
+            type: "bar",
+            text: otu_labels.slice(0,10).reverse(),
+            orientation: "h"
+        }
+
+        let barTrace = [barData]
+
+        let barLayout = {
+            title: "Top 10 Bacteria Cultures Found",
+            margin: {t: 30, l: 150}
+        }
+
+        Plotly.newPlot("bar", barTrace, barLayout)
+    });
 }
 
 // Function to draw the bubble chart
@@ -35,15 +67,14 @@ function InitDashboard() {
 
     let selector = d3.select("#selDataset");
     d3.json("samples.json").then(data => {
-        console.log(data);
 
         let sampleNames = data.names;
 
         sampleNames.forEach(sampleID => {
             selector.append("option")
                 .text(sampleID)
-                .property("value", sampleID)
-        })
+                .property("value", sampleID);
+        });
 
         let sampleID = sampleNames[0];
 
